67a050c754ce2eb414990f3e10494f87662ff086
Sakif Khan <sakifkhan1340@gmail.com>
2025-08-04 19:13:51 +0600

    Add the problem_5.js file

--- final.js
+++ final.js
@@ -48,4 +48,19 @@ if (fileName.startsWith("#") || fileName.endsWith(".pdf") || fileName.endsWith("
 } else {
     console.log("Delete");
 }
+
+
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse"  };
+//write your code here
+var email = student.name + student.roll + "." + student.department + "@ph.ac.bd";
+
+console.log(email);
+
+
+
+
+
     
